b6d4896ed68a387229488d1f98acaca92b5f697b
AlexeyTuhtiaev <alexey.tuhtiaev@gmail.com>
2019-12-18 20:04:31 +0300

    Bridge sample was added

--- Bridge/Program.cs
+++ Bridge/Program.cs
@@ -6,7 +6,16 @@ namespace Bridge
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            // создаем нового программиста, он работает с с++
+            Programmer freelancer = new FreelanceProgrammer(new CPPLanguage());
+            freelancer.DoWork();
+            freelancer.EarnMoney();
+            // пришел новый заказ, но теперь нужен c#
+            freelancer.Language = new CSharpLanguage();
+            freelancer.DoWork();
+            freelancer.EarnMoney();
+
+            Console.Read();
         }
-    }
+    }   
 }
